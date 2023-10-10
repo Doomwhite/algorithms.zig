aa49382aa470c6fede95f275b9e7ae58f7a65bb8
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-10-10 17:28:32 -0300

    Changes BubbleSort to use payload pointers

--- src/BubbleSort.zig
+++ src/BubbleSort.zig
@@ -1,16 +1,17 @@
 const std = @import("std");
 
-fn bubbleSort(arr: []i32) void {
-    var n = arr.len;
+pub fn bubbleSort(arr: []i32) void {
+    var n: usize = arr.len;
     while (n > 1) {
         var new_n: usize = 0;
-        for (arr[0 .. n - 1], 0..) |value, i| {
-            if (value > arr[i + 1]) {
-                std.mem.swap(i32, &arr[i], &arr[i + 1]);
+        for (arr[0 .. n - 1], arr[1..n], 0..) |*ptr, *next_ptr, i| {
+            const value: i32 = ptr.*;
+            const next_value: i32 = next_ptr.*;
+            if (value > next_value) {
+                std.mem.swap(i32, ptr, next_ptr);
                 new_n = i + 1;
             }
         }
-        // After each iteration the biggest element bubbles up the array
         n = new_n;
     }
 }
@@ -18,18 +19,7 @@ fn bubbleSort(arr: []i32) void {
 const arr_size = 7;
 
 test "Bubble sort" {
-    // const y: usize = 9;
-    // var haha = @constCast(&y);
-    // _ = haha;
-
-    // const unsorted_arr = [arr_size]i32{ 3, 4, 7, 9, 42, 69, 420 };
-    // for (unsorted_arr) |value| {
-    //     std.mem.swap(i32, value, 20);
-    //     std.debug.print("@constCast(&i32): {any}\n", .{@constCast(&i32)});
-    //     std.debug.print("value: {any}\n", .{value});
-    // }
-
-    var arr = [arr_size]i32{ 3, 4, 7, 9, 42, 69, 420 };
+    var arr = [arr_size]i32{ 4, 3, 42, 7, 69, 9, 420 };
     bubbleSort(&arr);
     const expected_arr = [arr_size]i32{ 3, 4, 7, 9, 42, 69, 420 };
 
