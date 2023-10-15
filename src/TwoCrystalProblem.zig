b4f283602a89c5f5507830aa5e06ab13a1e1f598
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-10-15 18:02:00 -0300

    feat: Improves the testign for the TwoCrystalProblem

--- src/TwoCrystalProblem.zig
+++ src/TwoCrystalProblem.zig
@@ -29,11 +29,16 @@ pub fn TwoCrystalProblem(arr: []bool) TwoCrystalProblemErrors!usize {
 }
 
 test "TwoCrystalProblem" {
+    var prng = std.rand.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.os.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
+    var expectedIndex: usize = @mod(prng.next(), 99999);
     var empty_arr = [_]bool{false} ** 100000;
-    var expectedIndex: usize = 80000;
     var arr = [_]bool{false} ** 100000;
-    for (arr[expectedIndex..]) |*haha| {
-        haha.* = true;
+    for (arr[expectedIndex..]) |*arr_value| {
+        arr_value.* = true;
     }
 
     try std.testing.expectEqual(expectedIndex, try TwoCrystalProblem(&arr));
