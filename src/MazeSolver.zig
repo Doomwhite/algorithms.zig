03fc07bc8f8c84e14b0cddfcc72517c6641b70c3
Adinelson Brühmüller <adinelson.bruhmuller@movtech.com.br>
2023-11-04 21:18:19 -0300

    feat(maze solver): finishes the maze solver implementation.

--- src/MazeSolver.zig
+++ src/MazeSolver.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const AutoHashMap = std.AutoHashMap;
+const ArrayList = std.ArrayList;
 
 pub const Point = struct {
     x: u8,
@@ -13,36 +14,25 @@ const directions = [_][2]i8{
     [2]i8{ 0, 1 },
 };
 
-pub fn walk(maze: [][]const u8, wall: u8, curr: Point, end: Point, seen: *AutoHashMap(Point, void), path: *AutoHashMap(Point, void)) !bool {
-    std.debug.print("curr: {any}\n", .{curr});
-    std.debug.print("maze: {any}\n", .{maze});
+pub fn walk(maze: [][]const u8, wall: u8, curr: Point, end: Point, seen: *AutoHashMap(Point, void), path: *ArrayList(Point)) !bool {
     // Off the map
     if ((curr.x < 0 or curr.x >= maze[0].len) or (curr.y < 0 or curr.y >= maze.len)) {
-        std.debug.print("Off the map\n", .{});
-        std.debug.print("(curr.x < 0 or curr.x >= maze[0].len) or (curr.y < 0 or curr.y >= maze.len): {any}\n", .{(curr.x < 0 or curr.x >= maze[0].len) or (curr.y < 0 or curr.y >= maze.len)});
         return false;
     }
 
     // On a wall
     if (maze[curr.y][curr.x] == wall) {
-        std.debug.print("maze[curr.y]: {any}\n", .{maze[curr.y]});
-        std.debug.print("On a wall\n", .{});
-        std.debug.print("maze[curr.y][curr.x] == wall: {any}\n", .{maze[curr.y][curr.x] == wall});
         return false;
     }
 
     // Found the end
     if (curr.x == end.x and curr.y == end.y) {
-        std.debug.print("Found the end\n", .{});
-        std.debug.print("curr.x == end.x and curr.y == end.y: {any}\n", .{curr.x == end.x and curr.y == end.y});
-        try path.put(curr, {});
+        try path.append(curr);
         return true;
     }
 
     // Hits a seen block
     if (seen.contains(curr)) {
-        std.debug.print("Hits a seen block\n", .{});
-        std.debug.print("seen.contains(curr): {any}\n", .{seen.contains(curr)});
         return false;
     }
 
@@ -50,12 +40,10 @@ pub fn walk(maze: [][]const u8, wall: u8, curr: Point, end: Point, seen: *AutoHa
 
     // pre
     try seen.put(curr, {});
-    try path.put(curr, {});
+    try path.append(curr);
 
     // recurse
     for (directions) |direction| {
-        std.debug.print("direction: {any}\n", .{direction});
-
         var new_x: u8 = undefined;
         if (curr.x == 0 and direction[0] < 0) {
             new_x = @as(u8, 0);
@@ -70,18 +58,13 @@ pub fn walk(maze: [][]const u8, wall: u8, curr: Point, end: Point, seen: *AutoHa
             new_y = @intCast(@as(i9, curr.y) + direction[1]);
         }
 
-        var new_point = Point{ .x = new_x, .y = new_y };
-        std.debug.print("curr: {any}\n", .{curr});
-        std.debug.print("new_point: {any}\n", .{new_point});
-        if (try walk(maze, wall, new_point, end, seen, path)) {
-            std.debug.print("after curr: {any}\n", .{curr});
-            std.debug.print("recursivo true\n", .{});
+        if (try walk(maze, wall, Point{ .x = new_x, .y = new_y }, end, seen, path)) {
             return true;
         }
     }
 
     // post
-    _ = path.remove(curr);
+    _ = path.pop();
 
     return false;
 }
@@ -90,51 +73,51 @@ pub fn solve(alloc: std.mem.Allocator, maze: [][]const u8, wall: u8, curr: Point
     var seen = AutoHashMap(Point, void).init(alloc);
     defer seen.deinit();
 
-    var path = AutoHashMap(Point, void).init(alloc);
+    var path = std.ArrayList(Point).init(alloc);
     defer path.deinit();
 
-    std.debug.print("before path: {any}\n", .{path});
-    const deu_boa = try walk(maze, wall, curr, end, &seen, &path);
-    std.debug.print("deu_boa: {any}\n", .{deu_boa});
-    std.debug.print("after path: {any}\n", .{path});
+    _ = try walk(maze, wall, curr, end, &seen, &path);
 
-    std.debug.print("path.count(): {any}\n", .{path.count()});
-    var iterator = path.keyIterator();
+    for (path.items) |path_item| {
+        std.debug.print("path_item: {any}\n", .{path_item});
+    }
 
-    std.debug.print("iterator.items: {any}\n", .{iterator.items});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[0]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[1]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[2]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[3]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[4]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[5]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[6]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[7]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[8]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[9]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[10]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[11]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[12]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[13]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[14]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[15]});
-    std.debug.print("iterator.items: {any}\n", .{iterator.items[16]});
-
-    var point = [_]Point{Point{ .x = 10, .y = 20 }};
-    return &point;
+    return path.toOwnedSlice();
 }
 
 test "Maze Solver" {
     var maze = [_][]const u8{
-        "############## #",
-        "#              #",
-        "# ##############",
+        "##### #",
+        "#   # #",
+        "# #   #",
+        "# #####",
+        "#     #",
+        "##### #",
     };
-    var start = Point{ .x = 14, .y = 0 };
-    var end = Point{ .x = 1, .y = 2 };
+
+    var start = Point{ .x = 5, .y = 0 };
+    var end = Point{ .x = 5, .y = 5 };
 
     var expected_result = [_]Point{
-        Point{ .x = 10, .y = 20 },
+        Point{ .x = 5, .y = 0 },
+        Point{ .x = 5, .y = 1 },
+        Point{ .x = 5, .y = 2 },
+        Point{ .x = 4, .y = 2 },
+        Point{ .x = 3, .y = 2 },
+        Point{ .x = 3, .y = 1 },
+        Point{ .x = 2, .y = 1 },
+        Point{ .x = 1, .y = 1 },
+        Point{ .x = 1, .y = 2 },
+        Point{ .x = 1, .y = 3 },
+        Point{ .x = 1, .y = 4 },
+        Point{ .x = 2, .y = 4 },
+        Point{ .x = 3, .y = 4 },
+        Point{ .x = 4, .y = 4 },
+        Point{ .x = 5, .y = 4 },
+        Point{ .x = 5, .y = 5 },
     };
-    try std.testing.expectEqualSlices(Point, &expected_result, try solve(std.testing.allocator, &maze, '#', start, end));
+    var result = try solve(std.testing.allocator, &maze, '#', start, end);
+    var allocator = std.testing.allocator;
+    defer allocator.free(result);
+    try std.testing.expectEqualSlices(Point, &expected_result, result);
 }
